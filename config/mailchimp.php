9ae1455bb73e97f561d7ad88ab5af8195cf9a42b
Svyatoslav <uptofly73@gmail.com>
2018-07-14 21:39:36 +0200

    [!!!][FEATURES] MailList model -resources

---
+++ config/mailchimp.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Cowboy
+ * Date: 14.07.2018
+ */
+
+return [
+    'api_key' => '3695a9c0ec9b9355f25844917abf070e-us18',
+    'api_url' => 'https://<dc>.api.mailchimp.com/3.0'
+];
